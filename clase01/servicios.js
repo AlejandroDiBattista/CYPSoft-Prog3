7ec8ecd6a7c361f0e3a08da08e5cca76b5fe6f61
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-03-27 01:49:41 -0300

    .

--- clase01/servicios.js
+++ clase01/servicios.js
@@ -1,6 +1,9 @@
+import dns from "dns"
+import ntpClient from 'ntp-client'
+
 // curl "https://jsonapi.org/" -H "accept: text/html"   CMD
 
-url = "https://jsonapi.org/"
+const url = "https://jsonapi.org/"
 
 // fetch(url, { method: "GET", headers: { accept: "text/html" }, body: null })
 
@@ -8,3 +11,14 @@ url = "https://jsonapi.org/"
 var respuesta = await fetch(url, { "method": "GET", "headers": { "accept": "text/html" } })
 var texto = await respuesta.text()
 console.log(texto)
+
+
+dns.resolve4('www.jsonapi.org', (err, addresses) => {
+    console.log(`Direcciones IP de 'www.jsonapi.org' es: \n\t${JSON.stringify(addresses)}`);
+});
+
+
+
+ntpClient.getNetworkTime("pool.ntp.org", 123, function (err, date) {
+    console.log(`La hora actual del servidor NTP es: \n\t${date}`);
+});
