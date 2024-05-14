00128f6e4ff695ebe3517f627e5899922e4f772d
nicocutone <nico44919323@gmail.com>
2024-05-14 17:55:28 -0300

    t.p1
    subido el t.p1

--- practicos/05 Cutone/tp1/main.jsx
+++ practicos/05 Cutone/tp1/main.jsx
@@ -1,11 +1,48 @@
 function App() {
     return (
         <div>
-            <h1>¡Hola, mundo!</h1>
-            <p>¡Bienvenidos a React!</p>
+            <h1>AGENDA DE CONTACTOS</h1>
+            <ContactList contacts={contacts} />
         </div>
-    )
+    );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const contacts = [
+    { id: 1, nombre: 'Nicolas', apellido: 'Cutone', telefono: '6582478' },
+    { id: 2, nombre: 'Joel', apellido: 'Corbalan', telefono: '6587459' },
+    { id: 3, nombre: 'Agustina', apellido: 'Acevedo', telefono: '6584498'  },
+    { id: 4, nombre: 'Lucas', apellido: 'Aguilar', telefono: '6985247' },
+    { id: 5, nombre: 'Facundo', apellido: 'vizcarra', telefono: '9856324' },
+    { id: 6, nombre: 'Pablo', apellido: 'Andrara', telefono: '9632587' },
+    { id: 7, nombre: 'Benjamin', apellido: 'Segura', telefono: '5829637'   },
+    { id: 8, nombre: 'Josue', apellido: 'Jaime', telefono: '7458965' },
+    { id: 9, nombre: 'Victor', apellido: 'Molina', telefono: '6985478' },
+    { id: 10, nombre: 'Jhonatan', apellido: 'Romano', telefono: '1254789' },
+    { id: 11, nombre: 'Diego', apellido: 'Tutor', telefono: '7453789' },
+    { id: 12, nombre: 'Cecilia', apellido: 'Rojas', telefono: '4638567' },
+
+];
+
+function ContactCard({ contact }) {
+    return (
+        <div  className="contact-card">
+            <h3>{contact.nombre} {contact.apellido}</h3>
+            <p>ID: {contact.id}</p>
+            <p>Teléfono: {contact.telefono}</p>
+        </div>
+    );
+}
+
+function ContactList({ contacts }) {
+    return (
+        <div className="contact-list">
+            {contacts.map(contact => (
+                <ContactCard key={contact.id} contact={contact} />
+            ))}
+        </div>
+    );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
