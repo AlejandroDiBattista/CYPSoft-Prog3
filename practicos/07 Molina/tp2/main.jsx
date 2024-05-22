54ded21c2a2ad8c84c51db37a6068d6525214b6b
Victor_Enrqiue_Molina <119140911+victor-desarrollador@users.noreply.github.com>
2024-05-22 09:00:25 -0300

    Update main.jsx
    actulizando

--- practicos/07 Molina/tp2/main.jsx
+++ practicos/07 Molina/tp2/main.jsx
@@ -13,7 +13,9 @@ function Contacto({ id, nombre, telefono, favorito, alCambiar }) {
     return (
         <div className="card">
             <span className="favorito">{favorito && "⭐"}</span>
+            <h2>Contacto</h2>
             <p className="nombre"> {nombre} <i>({id})</i> </p>
+            <h3>Telefono:</h3>
             <p>📞{telefono}</p>
             <button onClick={marcar}>{favorito ? "Desmarcar": "Marcar"}</button>
         </div>
